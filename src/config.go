9736e2b03d6f755b55be087c2d4cc74588a956a5
Stelzi79 <969932+Stelzi79@users.noreply.github.com>
2024-09-06 04:22:27 +0200

    refactured: split defaults from other config

--- src/config.go
+++ src/config.go
@@ -2,30 +2,54 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/spf13/viper"
 )
 
-func load_config() {
+var (
+	default_lbx          string
+	default_db           string
+	default_yaml_db_path string
+	yaml_db              string
+)
 
-	viper.SetDefault("default_lbx", "default_lbx not set")
-	viper.SetDefault("default_db", "default_db not set")
+func load_config() {
 
 	// Load configuration from config.yml file
 	// viper.SetConfigName("config") // name of config file (without extension)
 	// viper.SetConfigType("yaml")   // REQUIRED if the config file does not have the extension in the name
 
-	viper.AddConfigPath("../") // optionally look for config in the working directory
+	loadDefaults()
+
+	viper.SetDefault("yaml_db", default_yaml_db_path+strings.Replace(filepath.Base(os.Args[0]), ".exe", "", -1)+".yml-db")
+
+}
+
+func loadDefaults() {
+	viper.SetDefault("default_lbx", "default_lbx not set")
+	viper.SetDefault("default_db", "default_db not set")
+	viper.SetDefault("default_yaml_db_path", "default_yaml_db_path not set")
+
+	// Load configuration from config.yml file
+	// viper.SetConfigName("config") // name of config file (without extension)
+	// viper.SetConfigType("yaml")   // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("../")
 	viper.AddConfigPath(".")
 
 	if err := viper.ReadInConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			// Config file not found; ignore error if desired
+
 			fmt.Println("📢 Config file not found! Using and writing defaults to 'config.yml' ...")
 			viper.WriteConfigAs("config.yml")
 		} else {
-			panic(fmt.Errorf("📢 fatal error config file: %w", err)) // Config file was found but another error was produced
+			panic(fmt.Errorf("📢 fatal error config file: %w", err))
 		}
 	}
 
+	default_lbx = viper.GetString("default_lbx")
+	default_db = viper.GetString("default_db")
+	default_yaml_db_path = viper.GetString("default_yaml_db_path")
 }
